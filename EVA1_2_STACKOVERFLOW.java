24c8483c643eb190186a934e738e7f7825a259bb
OsielMarquez <124475050+OsielMarquez@users.noreply.github.com>
2023-02-04 20:15:09 -0600

    Add files via upload

---
+++ EVA1_2_STACKOVERFLOW.java
@@ -0,0 +1,17 @@
+
+package eva1_2_stackoverflow;
+
+//@author Marquez Aguilar Osiel Isaac
+
+public class EVA1_2_STACKOVERFLOW {
+
+    public static void main(String[] args) {
+      A();
+    }
+    public static void A(){
+        B();
+    }
+    public static void B(){
+        A();
+    }
+}
