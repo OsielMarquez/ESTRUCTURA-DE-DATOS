8f6b8c27df775ab10808e5ad18dfbde822e049a6
OsielMarquez <124475050+OsielMarquez@users.noreply.github.com>
2023-02-04 20:16:04 -0600

    Add files via upload

---
+++ EVA1_3_HEAP.java
@@ -0,0 +1,21 @@
+
+package eva1_3_heap;
+
+// @author Osiel Isaac Marquez Aguilar
+
+public class EVA1_3_HEAP {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+     Prueba miObj= new Prueba();
+        System.out.println(miObj);
+        Prueba miObj2 = new Prueba();
+        System.out.println(miObj2);
+        //miObj.x
+    }
+    
+    
+}
+class Prueba{
+    int x=100;
+}
